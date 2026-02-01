291df3a3ba4e59dc282ca271775091d93f358d5a
Jaime SL <jaime.salazarlahera@gmail.com>
2026-02-01 19:02:28 +0100

    Testing deploy

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from '@tanstack/react-start/config'
 
 export default defineConfig({
     ssr: false,
+
     server: {
         preset: 'static',
+    },
+
+    build: {
+        outDir: 'dist',
     },
 })
