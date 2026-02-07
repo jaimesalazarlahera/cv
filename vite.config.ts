32387a368a36dc3fbb9d7561a70a64f8cc8590c6
Jaime SL <jaime.salazarlahera@gmail.com>
2026-02-07 15:20:01 +0100

    Testing deploy

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ const config = defineConfig({
     outDir: 'dist/client',
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name]-[hash].js',
       },
     },
   },
